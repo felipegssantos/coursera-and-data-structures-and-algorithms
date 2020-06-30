dc22244f132b497de8fc38815e28eec10ce4f9c9
Felipe Santos <felipegssantos@gmail.com>
2020-06-29 22:36:45 -0300

    Detect whether a given tree is a binary tree

---
+++ data_structures/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import sys
+import threading
+
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**25)  # new thread will get stack of such size
+
+
+def is_binary_search_tree(tree):
+    # empty tree is considered valid
+    if not tree:
+        return True
+
+    # in-order traversal should return a sorted array
+    in_order = traverse_in_order(tree)
+    x1 = next(in_order)
+    for x2 in in_order:
+        if x2 < x1:
+            return False
+        x1 = x2
+    return True
+
+
+def traverse_in_order(tree):
+    def key(index): return tree[index][0]
+    def left(index): return tree[index][1]
+    def right(index): return tree[index][2]
+
+    stack = []
+    i = 0
+    while stack or i >= 0:
+        while i >= 0:
+            stack.append(i)
+            i = left(i)
+        if stack:
+            i = stack.pop()
+            yield key(i)
+            i = right(i)
+
+
+def key(tree, index):
+    return tree[index][0]
+
+
+def left_child(tree, index):
+    return tree[index][1]
+
+
+def right_child(tree, index):
+    return tree[index][2]
+
+
+def main():
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+    if is_binary_search_tree(tree):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
+
+threading.Thread(target=main).start()
---
+++ data_structures/week4_binary_search_trees/2_is_bst/test_is_bst.py
@@ -0,0 +1,45 @@
+from contextlib import contextmanager
+import sys
+
+import pytest
+
+from is_bst import is_binary_search_tree
+
+
+@contextmanager
+def replace_stdin(target):
+    orig = sys.stdin
+    sys.stdin = target
+    yield
+    sys.stdin = orig
+
+
+@pytest.fixture(params=['01', '02', '03', '04', '05', '06'])
+def index(request):
+    return request.param
+
+
+@pytest.fixture
+def tree(index):
+    with replace_stdin(open(f'tests/{index}')):
+        nodes = int(sys.stdin.readline().strip())
+        tree = []
+        for i in range(nodes):
+            tree.append(list(map(int, sys.stdin.readline().strip().split())))
+    return tree
+
+
+@pytest.fixture
+def expected(index):
+    with open(f'tests/{index}.a') as f:
+        answer = f.read().strip()
+    if answer == 'CORRECT':
+        return True
+    elif answer == 'INCORRECT':
+        return False
+    else:
+        raise ValueError(f'Wrong answer file for index={index}')
+
+
+def test_is_binary_search_tree(tree, expected):
+    assert is_binary_search_tree(tree) == expected
